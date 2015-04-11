6cf01b7da7aed0ecb09c9bc3aa1c9f660b576c1d
Wouter Bolsterlee <uws@xs4all.nl>
2015-04-12 00:30:06 +0200

    Expand setup script

--- setup.py
+++ setup.py
@@ -1,7 +1,27 @@
+import os
 from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as fp:
+    long_description = fp.read()
 
 setup(
     name='qualname',
+    description="__qualname__ emulation for older Python versions",
+    long_description=long_description,
     version='0.0.1',
+    author="Wouter Bolsterlee",
+    author_email="uws@xs4all.nl",
+    url='https://github.com/wbolster/qualname',
+    license='BSD',
     py_modules=['qualname'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
